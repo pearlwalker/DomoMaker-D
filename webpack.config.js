c317625c2f4b9c99deb7a3a4db313beaf98067b2
pearlwalker <paw7293@rit.edu>
2025-12-05 14:35:22 -0500

    Replaced client.js entry point with multiple .jsx entry points

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
 module.exports = {
-    entry: './client/client.js',
+    entry: {
+        app: './client/maker.jsx',
+        login: './client/login.jsx',
+    },
     module: {
         rules: [
             {
