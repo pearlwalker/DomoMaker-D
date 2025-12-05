c84aae16047ee3f212f54cc4b07fa96e6e22d9c3
pearlwalker <paw7293@rit.edu>
2025-12-05 14:28:53 -0500

    Copied sendPost function from client.js into helper.js

--- client/helper.js
+++ client/helper.js
@@ -2,3 +2,24 @@ const handleError = (message) => {
     document.getElementById('errorMessage').textContent = message;
     document.getElementById('domoMessage').classList.remove('hidden');
 };
+
+const sendPost = async (url, data) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  const result = await response.json();
+  document.getElementById('domoMessage').classList.add('hidden');
+
+  if(result.redirect) {
+    window.location = result.redirect;
+  }
+
+  if(result.error) {
+    handleError(result.error);
+  }
+};
