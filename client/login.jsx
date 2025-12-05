925581125987fbcc4b8c863c97de67b60d8b7ccb
pearlwalker <paw7293@rit.edu>
2025-12-05 14:46:32 -0500

    Prevent default and hide errors in handler functions

--- client/login.jsx
+++ client/login.jsx
@@ -1,15 +1,16 @@
-/* eslint-disable no-undef */
-/* eslint-disable linebreak-style */
+/* eslint-disable */
 const helper = require('./helper.js');
 const React = require('react');
 const { createRoot } = require('react-dom/client');
 
 const handleLogin = (e) => {
-
+    e.preventDefault();
+    helper.hideError();
 };
 
 const handleSignup = (e) => {
-
+    e.preventDefault();
+    helper.hideError();
 };
 
 const LoginWindow = (props) => {
