1da3dfcb8fc7ede7dd55bb0de0287b6eadd16bf2
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-12-06 19:12:27 -0500

    Created empty handleDomo function

--- client/maker.jsx
+++ client/maker.jsx
@@ -2,3 +2,7 @@ const helper = require('./helper.js');
 const React = require('react');
 const { useState, useEffect } = React;
 const { createRoot } = require('react-dom/client');
+
+const handleDomo = (e, onDomoAdded) => {
+
+};
